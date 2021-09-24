85d28c83e065a128ae29778c5c1b6be2663cc32c
Daan van Berkel <daan.van.berkel@alliander.com>
2021-09-24 17:02:54 +0200

    define directions

--- src/turing/mod.rs
+++ src/turing/mod.rs
@@ -4,6 +4,12 @@ enum Symbol {
     NonBlank,
 }
 
+#[derive(Debug, PartialEq, Eq)]
+enum Direction {
+    Left,
+    Right,
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -18,5 +24,17 @@ mod tests {
     fn distinct_symbols_are_distinct() {
         assert_ne!(Symbol::Blank, Symbol::NonBlank);
         assert_ne!(Symbol::NonBlank, Symbol::Blank);
+    }
+
+    #[test]
+    fn same_direction_are_equal() {
+        assert_eq!(Direction::Left, Direction::Left);
+        assert_eq!(Direction::Right, Direction::Right);
+    }
+
+    #[test]
+    fn distinct_direction_are_distinct() {
+        assert_ne!(Direction::Left, Direction::Right);
+        assert_ne!(Direction::Right, Direction::Left);
     }
 }
