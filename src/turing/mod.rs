6d674ca396d0538267b6d54193b67d3bbba8bd84
Daan van Berkel <daan.van.berkel@alliander.com>
2021-09-26 10:29:36 +0200

    implement a Turing machine

--- src/turing/mod.rs
+++ src/turing/mod.rs
@@ -1,36 +1,151 @@
 use std::collections::HashMap;
+use std::ops::{Index, IndexMut};
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq, Copy, Clone, Hash)]
 enum Symbol {
     Blank,
     NonBlank,
 }
 
-#[derive(Debug, PartialEq, Eq)]
+impl Default for Symbol {
+    fn default() -> Self {
+        Symbol::Blank
+    }
+}
+
+impl Default for &Symbol {
+    fn default() -> Self {
+        &Symbol::Blank
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, Copy, Clone)]
 enum Direction {
     Left,
     Right,
 }
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq, Copy, Clone, Hash)]
 enum State {
     Halt,
     Number(u8),
 }
 
+impl State {
+    fn halted(&self) -> bool {
+        matches!(self, State::Halt)
+    }
+}
+
 type Program = HashMap<Key, Action>;
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq, Copy, Clone, Hash)]
 struct Key {
     state: State,
     symbol: Symbol,
 }
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq, Copy, Clone)]
 struct Action {
     symbol: Symbol,
     direction: Direction,
     state: State,
+}
+struct Tape {
+    right: Vec<Symbol>,
+    left: Vec<Symbol>,
+}
+
+impl Tape {
+    fn empty() -> Tape {
+        Tape {
+            right: Vec::new(),
+            left: Vec::new(),
+        }
+    }
+}
+
+type Head = i128;
+
+fn move_to(head: &Head, direction: &Direction) -> Head {
+    match direction {
+        Direction::Left => head - 1,
+        Direction::Right => head + 1,
+    }
+}
+
+impl Index<Head> for Tape {
+    type Output = Symbol;
+
+    fn index(&self, index: Head) -> &Self::Output {
+        if index >= 0i128 {
+            self.right.get(index as usize).unwrap_or_default()
+        } else {
+            self.left.get((-index) as usize).unwrap_or_default()
+        }
+    }
+}
+
+impl IndexMut<Head> for Tape {
+    fn index_mut(&mut self, index: Head) -> &mut Self::Output {
+        if index >= 0i128 {
+            let i = index as usize;
+            if i >= self.right.len() {
+                self.right.insert(i, Symbol::Blank)
+            }
+            self.right.index_mut(index as usize)
+        } else {
+            let i = (-index) as usize;
+            if i >= self.left.len() {
+                self.left.insert(i, Symbol::Blank)
+            }
+            self.left.index_mut((-index) as usize)
+        }
+    }
+}
+
+struct Machine {
+    head: Head,
+    tape: Tape,
+    program: Program,
+    state: State,
+}
+
+impl Machine {
+    fn new(start_state: State, program: Program) -> Self {
+        Self {
+            head: 0i128,
+            tape: Tape::empty(),
+            program: program,
+            state: start_state,
+        }
+    }
+
+    fn step(&mut self) {
+        if !self.state.halted() {
+            let key = Key {
+                state: self.state,
+                symbol: self.tape[self.head],
+            };
+            match self.program.get(&key) {
+                None => self.state = State::Halt,
+                Some(action) => {
+                    self.tape[self.head] = action.symbol;
+                    self.head = move_to(&self.head, &action.direction);
+                    self.state = action.state;
+                }
+            }
+        }
+    }
+
+    fn run(&mut self, maximum_steps: u128) -> u128 {
+        let mut steps_taken: u128 = 0u128;
+        while !self.state.halted() && steps_taken < maximum_steps {
+            self.step();
+            steps_taken += 1;
+        }
+        steps_taken
+    }
 }
 
 #[cfg(test)]
@@ -65,5 +180,78 @@ mod tests {
     fn distinct_states_are_distinct() {
         assert_ne!(State::Halt, State::Number(0u8));
         assert_ne!(State::Number(0u8), State::Halt);
+    }
+
+    #[test]
+    fn empty_tape_contains_blanks() {
+        let tape = Tape::empty();
+        let head: Head = 0i128;
+
+        assert_eq!(tape[head], Symbol::Blank);
+    }
+
+    #[test]
+    fn tape_can_be_written_to() {
+        let mut tape = Tape::empty();
+        let head: Head = 0i128;
+
+        tape[head] = Symbol::NonBlank;
+
+        assert_eq!(tape[head], Symbol::NonBlank);
+    }
+
+    #[test]
+    fn a_simple_machine_can_be_run() {
+        let mut program: Program = HashMap::new();
+        program.insert(
+            Key {
+                state: State::Number(0),
+                symbol: Symbol::Blank,
+            },
+            Action {
+                symbol: Symbol::NonBlank,
+                direction: Direction::Right,
+                state: State::Number(1),
+            },
+        );
+        program.insert(
+            Key {
+                state: State::Number(0),
+                symbol: Symbol::NonBlank,
+            },
+            Action {
+                symbol: Symbol::NonBlank,
+                direction: Direction::Left,
+                state: State::Halt,
+            },
+        );
+        program.insert(
+            Key {
+                state: State::Number(1),
+                symbol: Symbol::Blank,
+            },
+            Action {
+                symbol: Symbol::NonBlank,
+                direction: Direction::Left,
+                state: State::Number(0),
+            },
+        );
+        program.insert(
+            Key {
+                state: State::Number(1),
+                symbol: Symbol::NonBlank,
+            },
+            Action {
+                symbol: Symbol::NonBlank,
+                direction: Direction::Right,
+                state: State::Halt,
+            },
+        );
+        let start = State::Number(0);
+        let mut machine = Machine::new(start, program);
+
+        let steps = machine.run(10);
+
+        assert_eq!(steps, 3);
     }
 }
